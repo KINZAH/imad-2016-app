a250ce1fcee587325fb3363efc8f762bd7c3d5d3
KINZAH <tanzahfathima@gmail.com>
2016-11-21 15:34:19 +0530

    [imad-console] Updates ui/main.js

---
+++ ui/main.js
@@ -0,0 +1,32 @@
+// Submit username/password to login
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () { 
+    
+    // Create a request object
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+           // Take some action
+           if (request.status === 200) {
+              alert('Logged in successfully');
+              } else if (request.status === 403) {
+                  alert('Username/passsword is incorrect');
+              } else if(request.status === 500) { 
+                  alert('Something went wrong on the server');
+              }
+      }
+      // Not done yet
+   };
+    
+   // Make the request
+   var username = document.getElementById('username').value;
+   var password = document.getElementById('password').value;
+   console.log('username');
+   console.log('password');
+   request.open('POST', 'http://tanrib.imad.hasura-app.io/login', true);
+   request.setRequestHeader('Content-Type', 'application/json');
+   request.send(JSON.stringify({username: username, password: password}));
+   
+};
