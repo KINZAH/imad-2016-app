d62726eeeb1abd66687153ab372187937a43cff7
KINZAH <tanzahfathima@gmail.com>
2016-11-23 16:51:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var config = {
     database: 'kinzah',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: pocess.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD
 };
 
 var app = express();
