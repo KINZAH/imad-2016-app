61cff46174388a56e0f793a2846fc551bf24d6af
KINZAH <tanzahfathima@gmail.com>
2016-11-23 17:57:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,7 +95,6 @@ app.post('/create-user', function (req, res) {
    });
 });
 
-
 app.post('/login', function (req, res) {
    var username = req.body.username;
    var password = req.body.password;
