4563a1d05cd529329e88e32f7cb9a522b52bffb7
KINZAH <tanzahfathima@gmail.com>
2016-11-23 12:07:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,30 @@
+
+function loadLoginForm () {
+var loginHtml = `
+     <h3>Login/Register to unlock awesome features</h3>
+     <input type="text" id="username" placeholder="username" />
+     <input type="password" id="password" />
+     <br/><br/>
+     <input type="submit" id="login_btn" value="Login" />
+     <input type="submit" id="register_btn" value="Register" />
+     `;
+document.getElementById('login_area').innerHTML = loginHtml;
+}
+
+function loadLogin () {
+// Check if the user is already logged in
+var request = new XMLHttpRequest();
+request.onreadystatechange = function () {
+    if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+            loadLoggedInUser(this.responseText);
+        } else {
+            loadLoginForm();
+        }
+    }
+  };
+}    
+
 // Submit username/password to login
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () { 
@@ -30,3 +57,11 @@ submit.onclick = function () {
    request.send(JSON.stringify({username: username, password: password}));
    
 };
+
+loadLogin();
+
+// Now this is something that we could have directly done on the server-side using templating too!
+loadArticles();
+
+
+
