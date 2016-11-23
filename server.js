da15260488eebb94fadd49457bbf5cd5cb9ed739
KINZAH <tanzahfathima@gmail.com>
2016-11-23 16:50:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 
 var config = {
-    user: 'tanrib',
+    user: '	kinzah',
     database: 'kinzah',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: process.env.DB_PASSWORD
+    password: pocess.env.DB_PASSWORD
 };
 
 var app = express();
