8b95c63fe04210e6e979f7788efda8401570997f
KINZAH <tanzahfathima@gmail.com>
2016-11-23 18:32:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -194,7 +194,7 @@ app.get('/submit-name', function(req , res) { // URL: /submit-name?name-xxxxx
     res.send(JSON.stringify(names));
 });
 
-app.get('/articles/:articleName',function (req,res) {
+app.get('/:articleName',function (req,res) {
     pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function(err,result) {
         if(err){
             res.status(500).send(err.toString());
