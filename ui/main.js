5d0955436a7a22600591ce1d3e2f3d9496d01003
KINZAH <tanzahfathima@gmail.com>
2016-11-26 21:27:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -116,6 +116,24 @@ function loadLogin () {
     request.open('GET', '/check-login', true);
     request.send(null);
 }
+
+
+//site visit counter
+function getCounter(){
+var request = new XMLHttpRequest();
+request.onreadystatechange = function() {
+    if(request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }
+};
+ request.open('GET', '/counter', true);
+    request.send(null);
+}
+
 function loadArticles () {
         // Check if the user is already logged in
     var request = new XMLHttpRequest();
@@ -141,6 +159,8 @@ function loadArticles () {
     request.open('GET', '/get-articles', true);
     request.send(null);
 }
+
+getCounter();
 // The first thing to do is to check if the user is logged in!
 loadLogin();
 // Now this is something that we could have directly done on the server-side using templating too!
