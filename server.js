196d5d638ac9bf67587489c1bcf1278a84e0db0b
KINZAH <tanzahfathima@gmail.com>
2016-11-26 20:37:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -294,7 +294,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/ui/blog1.png', function (req, res) {
+app.get('/ui/blog2.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'blog1.png'));
 });
 
