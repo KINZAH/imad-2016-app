8697c2780a8b5d89c2a73760c3e11936c2c0484c
KINZAH <tanzahfathima@gmail.com>
2016-11-26 21:27:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -260,6 +260,12 @@ app.post('/submit-comment/:articleName', function (req, res) {
     }
 });
 
+var counter = 0;
+app.get('/counter',function(req,res){
+    counter = counter+1;
+   res.send(counter.toString()); 
+});
+
 
 app.get('/articles/:articleName',function (req,res) {
     pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function(err,result) {
